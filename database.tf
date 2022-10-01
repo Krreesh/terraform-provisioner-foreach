61d5fda1f5181e0cfe4bbbad0cc5178172ee2778
krreesh <krreesh@outlook.com>
2022-10-01 10:36:52 +0000

    first commit

---
+++ database.tf
@@ -0,0 +1,16 @@
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 5
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  db_name              = "mydb"
+  username             = "foo"
+  password             = "${file("../dbpass.txt")}"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = "true"
+
+  provisioner "local-exec" {
+    command = "cat ../dbpass.txt"
+  }
+}
